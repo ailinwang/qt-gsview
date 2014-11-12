7a9b2dfc0d608f0f661b80f19f92fc513af0324a
fredrossperry <fredrossperry@gmail.com>
2014-11-12 13:04:36 -0800

    extract pages: add format list

--- ExtractPagesDialog.cpp
+++ ExtractPagesDialog.cpp
@@ -2,6 +2,55 @@
 #include "ui_ExtractPagesDialog.h"
 #include "Window.h"
 
+#include <QMessageBox>
+
+std::vector<std::string> devices = {
+    "svg",
+    "pnm",
+    "pclbitmap",
+    "pwg",
+    "bmp16",  /* Add mupdf devices before this one */
+    "bmp16m",
+    "bmp256",
+    "bmp32b",
+    "bmpgray",
+    "bmpmono",
+    "eps2write",
+    "jpeg",
+    "jpegcmyk",
+    "jpeggray",
+    "pamcmyk32",
+    "pamcmyk4",
+    "pbm",
+    "pgm",
+    "png16",
+    "png16m",
+    "png256",
+    "pngalpha",
+    "pnggray",
+    "pngmono",
+    "psdcmyk",
+    "psdrgb",  /* Add single page gs devices before this device */
+    "pdfwrite",
+    "ps2write",
+    "pxlcolor",
+    "pxlmono",
+    "tiff12nc",
+    "tiff24nc",
+    "tiff32nc",
+    "tiff64nc",
+    "tiffcrle",
+    "tiffg3",
+    "tiffg32d",
+    "tiffg4",
+    "tiffgray",
+    "tifflzw",
+    "tiffpack",
+    "tiffsep",
+    "txtwrite",
+    "xpswrite"
+};
+
 ExtractPagesDialog::ExtractPagesDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ExtractPagesDialog)
@@ -23,10 +72,14 @@ void ExtractPagesDialog::run(Window *win)
 
     //  set up page list
     ui->pageList->clear();
-    int pageCount = m_window->document()->GetPageCount();
-    for (int i=0; i<pageCount; i++)
-    {
+    for (int i=0; i<m_window->document()->GetPageCount(); i++)
         ui->pageList->addItem(QString::number(i+1));
+
+    //  set up the format list
+    for (int i=0; i<(int)devices.size(); i++)
+    {
+        std::string device = devices.at(i);
+        ui->formatList->addItem(QString(device.c_str()));
     }
 
     show();
@@ -35,11 +88,6 @@ void ExtractPagesDialog::run(Window *win)
 void ExtractPagesDialog::on_cancelButton_clicked()
 {
     close();
-}
-
-void ExtractPagesDialog::on_extractButton_clicked()
-{
-    //  todo - the actual extraction
 }
 
 void ExtractPagesDialog::on_allButton_clicked()
@@ -68,4 +116,24 @@ void ExtractPagesDialog::on_noneButton_clicked()
     //  select no pages
     for (int i=0; i<m_window->document()->GetPageCount(); i++)
         ui->pageList->item(i)->setSelected(false);
+}
+
+void ExtractPagesDialog::on_extractButton_clicked()
+{
+    //  the actual extraction
+
+    //  must have pages selected
+    if (ui->pageList->selectedItems().size()<=0)
+    {
+        QMessageBox::information(NULL, tr(""), tr("You must select one or more pages."));
+        return;
+    }
+
+    //  must have a format selected
+    if (ui->formatList->selectedItems().size()<=0)
+    {
+        QMessageBox::information(NULL, tr(""), tr("You must select a format."));
+        return;
+    }
+
 }
