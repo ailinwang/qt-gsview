00a8b048dd245a32f69ead8b118a46b53f6f93fd
fredrossperry <fredrossperry@gmail.com>
2014-11-12 14:09:41 -0800

    more work on extractpagesdialog

--- ExtractPagesDialog.cpp
+++ ExtractPagesDialog.cpp
@@ -4,51 +4,59 @@
 
 #include <QMessageBox>
 
-std::vector<std::string> devices = {
-    "svg",
-    "pnm",
-    "pclbitmap",
-    "pwg",
-    "bmp16",  /* Add mupdf devices before this one */
-    "bmp16m",
-    "bmp256",
-    "bmp32b",
-    "bmpgray",
-    "bmpmono",
-    "eps2write",
-    "jpeg",
-    "jpegcmyk",
-    "jpeggray",
-    "pamcmyk32",
-    "pamcmyk4",
-    "pbm",
-    "pgm",
-    "png16",
-    "png16m",
-    "png256",
-    "pngalpha",
-    "pnggray",
-    "pngmono",
-    "psdcmyk",
-    "psdrgb",  /* Add single page gs devices before this device */
-    "pdfwrite",
-    "ps2write",
-    "pxlcolor",
-    "pxlmono",
-    "tiff12nc",
-    "tiff24nc",
-    "tiff32nc",
-    "tiff64nc",
-    "tiffcrle",
-    "tiffg3",
-    "tiffg32d",
-    "tiffg4",
-    "tiffgray",
-    "tifflzw",
-    "tiffpack",
-    "tiffsep",
-    "txtwrite",
-    "xpswrite"
+class device_t
+{
+public:
+    int index;
+    QString name;
+    QString label;
+};
+
+std::vector<device_t> devices = {
+    {0,"svg","svg"},
+    {1,"pnm","pnm"},
+    {2,"pclbitmap","pclbitmap"},
+    {3,"pwg","pwg"},
+    {4,"bmp16","bmp16"},            /* Add mupdf devices before this one */
+    {5,"bmp16m","bmp16m"},
+    {6,"bmp256","bmp256"},
+    {7,"bmp32b","bmp32b"},
+    {8,"bmpgray","bmpgray"},
+    {9,"bmpmono","bmpmono"},
+    {10,"eps2write","eps2write"},
+    {11,"jpeg","jpeg"},
+    {12,"jpegcmyk","jpegcmyk"},
+    {13,"jpeggray","jpeggray"},
+    {14,"pamcmyk32","pamcmyk32"},
+    {15,"pamcmyk4","pamcmyk4"},
+    {16,"pbm","pbm"},
+    {17,"pgm","pgm"},
+    {18,"png16","png16"},
+    {19,"png16m","png16m"},
+    {20,"png256","png256"},
+    {21,"pngalpha","pngalpha"},
+    {22,"pnggray","pnggray"},
+    {23,"pngmono","pngmono"},
+    {24,"psdcmyk","psdcmyk"},
+    {25,"psdrgb  ","psdrgb  "},         /* Add single page gs devices before this device */
+    {26,"pdfwrite","pdfwrite"},
+    {27,"ps2write","ps2write"},
+    {28,"pxlcolor","pxlcolor"},
+    {29,"pxlmono","pxlmono"},
+    {30,"tiff12nc","tiff12nc"},
+    {31,"tiff24nc","tiff24nc"},
+    {32,"tiff32nc","tiff32nc"},
+    {33,"tiff64nc","tiff64nc"},
+    {34,"tiffcrle","tiffcrle"},
+    {35,"tiffg3","tiffg3"},
+    {36,"tiffg32d","tiffg32d"},
+    {37,"tiffg4","tiffg4"},
+    {38,"tiffgray","tiffgray"},
+    {39,"tifflzw","tifflzw"},
+    {40,"tiffpack","tiffpack"},
+    {41,"tiffsep","tiffsep"},
+    {42,"txtwrite","txtwrite"},
+    {43,"xpswrite","xpswrite"},
 };
 
 ExtractPagesDialog::ExtractPagesDialog(QWidget *parent) :
@@ -78,8 +86,8 @@ void ExtractPagesDialog::run(Window *win)
     //  set up the format list
     for (int i=0; i<(int)devices.size(); i++)
     {
-        std::string device = devices.at(i);
-        ui->formatList->addItem(QString(device.c_str()));
+        device_t device = devices.at(i);
+        ui->formatList->addItem(QString(device.label));
     }
 
     show();
