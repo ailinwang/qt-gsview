2a521f1e5db5c46734b273d083c0e29bb9a0b882
fredrossperry <fredrossperry@gmail.com>
2014-11-24 13:11:43 -0800

    take out testing code

--- main.cpp
+++ main.cpp
@@ -8,18 +8,17 @@
 int main(int argc, char *argv[])
 {
     //  create a GUI app and run it
-//    GSViewApp app (argc, argv);
+    GSViewApp app (argc, argv);
 
-    //  for testing purposes, skip
-    QApplication app(argc, argv);
-    Window *newWindow = new Window();
-    QDesktopWidget dw;
-    int x=dw.width()*0.85;
-    int y=dw.height()*0.85;
-    newWindow->setFixedSize(x,y);
-    newWindow->show();
-    newWindow->OpenFile("/Users/fredross-perry/Desktop/CM District Admin Overview.pdf");
-
+//    //  for testing purposes, skip
+//    QApplication app(argc, argv);
+//    Window *newWindow = new Window();
+//    QDesktopWidget dw;
+//    int x=dw.width()*0.85;
+//    int y=dw.height()*0.85;
+//    newWindow->setFixedSize(x,y);
+//    newWindow->show();
+//    newWindow->OpenFile("/Users/fredross-perry/Desktop/CM District Admin Overview.pdf");
 
     return app.exec();
 }
