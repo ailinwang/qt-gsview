71edf0b4c5f00aaa6df3ae4fa4b3c5e66a1b9535
Fred Ross-Perry <fredrossperry@gmail.com>
2015-02-03 15:12:44 -0800

    update install script to refresh app

--- linux-deploy/build.sh
+++ linux-deploy/build.sh
@@ -4,4 +4,11 @@
 this_dir=`dirname "${0}"`
 cd "$this_dir"
 
+# refresh release build parts
+rm -f ./packages/com.artifex.gsview/data/gsview
+rm -rf ./packages/com.artifex.gsview/data/apps
+mkdir ./packages/com.artifex.gsview/data/apps
+cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview
+cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
+
 ./binarycreator -c config/config.xml -p packages gsview-installer
