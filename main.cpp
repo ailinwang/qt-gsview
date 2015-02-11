8284a0ed3360ed4bf2ed6e96aa2acef7097f03b2
fredrossperry <fredrossperry@gmail.com>
2015-02-11 14:53:52 -0800

    hmmm...

--- main.cpp
+++ main.cpp
@@ -7,29 +7,26 @@
 
 int main(int argc, char *argv[])
 {
-    if (true)
-    {
-        //  create a GUI app and run it
-        GSViewApp app (argc, argv);
+#if 1
 
-        //  don't quit when the last file is closed.
-        app.setQuitOnLastWindowClosed(false);
+    //  create a GUI app and run it
+    GSViewApp app (argc, argv);
 
-        return app.exec();
-    }
-    else
-    {
-        //  for testing purposes
-        QApplication app(argc, argv);
-        Window *newWindow = new Window();
-        QDesktopWidget dw;
-        int x=dw.width()*0.85;
-        int y=dw.height()*0.85;
-        newWindow->setFixedSize(x,y);
-        newWindow->show();
-        newWindow->OpenFile("/Users/fredross-perry/Desktop/CM District Admin Overview.pdf");
+    //  don't quit when the last file is closed.
+    app.setQuitOnLastWindowClosed(false);
+
+    return app.exec();
+
+#else
+
+    //  for testing purposes
+    QApplication app(argc, argv);
+    Window *newWindow = new Window();
+    newWindow->show();
+    newWindow->OpenFile("/Users/fredross-perry/Desktop/CM District Admin Overview.pdf");
 //        newWindow->OpenFile("/home/fred/Desktop/sample files/pdf/CM District Admin Overview.pdf");
-        return app.exec();
-    }
+    return app.exec();
+
+#endif
 
 }
