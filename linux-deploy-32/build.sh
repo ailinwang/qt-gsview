dd5f9fc7953d234e7bdf1951c145d8116d584c4a
fredrossperry <fredrossperry@gmail.com>
2015-03-27 10:52:08 -0700

    update linux installer build script to clean up more

--- linux-deploy-32/build.sh
+++ linux-deploy-32/build.sh
@@ -4,16 +4,16 @@
 this_dir=`dirname "${0}"`
 cd "$this_dir"
 
-#  refresh release build parts
-#  for now leave libs and platforms alone
+# get latest built apps
 rm -f ./packages/com.artifex.gsview/data/gsview
 rm -rf ./packages/com.artifex.gsview/data/apps
-#rm -rf ./packages/com.artifex.gsview/data/libs
-#rm -rf ./packages/com.artifex.gsview/data/platforms
 cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview
 cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
-#cp -r ../build/release/libs ./packages/com.artifex.gsview/data/libs
-#cp -r ../build/release/platforms ./packages/com.artifex.gsview/data/platforms
 
+#  build the installer
 ./binarycreator -c config/config.xml -p packages GSView-6.0-Installer-32
+
+#  delete the now-not-needed apps
+rm -f ./packages/com.artifex.gsview/data/gsview
+rm -rf ./packages/com.artifex.gsview/data/apps
 
--- linux-deploy/build.sh
+++ linux-deploy/build.sh
@@ -4,15 +4,16 @@
 this_dir=`dirname "${0}"`
 cd "$this_dir"
 
-# refresh release build parts
+# get latest built apps
 rm -f ./packages/com.artifex.gsview/data/gsview
 rm -rf ./packages/com.artifex.gsview/data/apps
-#rm -rf ./packages/com.artifex.gsview/data/libs
-#rm -rf ./packages/com.artifex.gsview/data/platforms
 cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview
 cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
-#cp -r ../build/release/libs ./packages/com.artifex.gsview/data/libs
-#cp -r ../build/release/platforms ./packages/com.artifex.gsview/data/platforms
 
+#  build the installer
 ./binarycreator -c config/config.xml -p packages GSView-6.0-Installer
+
+#  delete the now-not-needed apps
+rm -f ./packages/com.artifex.gsview/data/gsview
+rm -rf ./packages/com.artifex.gsview/data/apps
 
