c4e7eebca694fd13cfebe4c53ef98919a1c59fa2
fredrossperry <fredrossperry@gmail.com>
2015-03-27 09:51:03 -0700

    modify 64-bit linux installer to provide certain shared libraries

--- linux-deploy/build.sh
+++ linux-deploy/build.sh
@@ -7,12 +7,12 @@ cd "$this_dir"
 # refresh release build parts
 rm -f ./packages/com.artifex.gsview/data/gsview
 rm -rf ./packages/com.artifex.gsview/data/apps
-rm -rf ./packages/com.artifex.gsview/data/libs
-rm -rf ./packages/com.artifex.gsview/data/platforms
+#rm -rf ./packages/com.artifex.gsview/data/libs
+#rm -rf ./packages/com.artifex.gsview/data/platforms
 cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview
 cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
-cp -r ../build/release/libs ./packages/com.artifex.gsview/data/libs
-cp -r ../build/release/platforms ./packages/com.artifex.gsview/data/platforms
+#cp -r ../build/release/libs ./packages/com.artifex.gsview/data/libs
+#cp -r ../build/release/platforms ./packages/com.artifex.gsview/data/platforms
 
 ./binarycreator -c config/config.xml -p packages GSView-6.0-Installer
 
