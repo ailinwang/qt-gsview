7cbce3a452b6b9e34fe8b0bc40bb5dd2832def8b
fredrossperry <fredrossperry@gmail.com>
2015-03-31 18:26:50 -0700

    fix mac deploy installer script

--- mac-deploy-qtifw/build.sh
+++ mac-deploy-qtifw/build.sh
@@ -8,7 +8,7 @@ rm -rf ./GSView-Installer
 
 #  update our copy of the release build
 rm -rf ./packages/com.artifex.gsview/data/gsview.app
-cp -r ../build/release/gsview.app ./packages/com.artifex.gsview/data/gsview.app
+ditto ../build/release/gsview.app ./packages/com.artifex.gsview/data/gsview.app
 
 #  build the installer
 ./binarycreator -c config/config.xml -p packages GSView-Installer
