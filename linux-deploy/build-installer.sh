a781ac30f4b8ca9952d607746a9cffdc2dcfa95f
Fred Ross-Perry <fredrossperry@gmail.com>
2015-04-03 14:29:19 -0700

    rework 64-bit installer to use elfpatch and a redirect script for launching.

--- linux-deploy/build-installer.sh
+++ linux-deploy/build-installer.sh
@@ -5,16 +5,21 @@ this_dir=`dirname "${0}"`
 cd "$this_dir"
 
 # get latest built apps
-rm -f ./packages/com.artifex.gsview/data/gsview
+rm -f ./packages/com.artifex.gsview/data/gsview.exe
 rm -rf ./packages/com.artifex.gsview/data/apps
-cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview
+cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview.exe
 cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
+
+#  patch the exe and set permissions
+patchelf --set-interpreter ./libs/ld-linux-x86-64.so.2 ./packages/com.artifex.gsview/data/gsview.exe 
+chmod 755 ./packages/com.artifex.gsview/data/gsview.exe
+chmod 755 ./packages/com.artifex.gsview/data/gsview
 
 #  build the installer
 ./binarycreator -c config/config.xml -p packages GSView-6.0-Installer
 chmod 755 GSView-6.0-Installer
 
 #  delete the now-not-needed apps
-rm -f ./packages/com.artifex.gsview/data/gsview
+rm -f ./packages/com.artifex.gsview/data/gsview.exe
 rm -rf ./packages/com.artifex.gsview/data/apps
 
