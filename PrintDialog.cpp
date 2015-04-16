7c921071526c00222f2b3f166acf2a13f683a464
fredrossperry <fredrossperry@gmail.com>
2015-04-16 09:13:10 -0700

    add debugging statements to print dialog.  temporary for mvrhel

--- PrintDialog.cpp
+++ PrintDialog.cpp
@@ -9,58 +9,74 @@
 #include <QMessageBox>
 #include <QTimer>
 #include <QPainter>
+#include <QTextStream>
 
 PrintDialog::PrintDialog(QWidget *parent, int maxPages, int currentPage, QPrinter *printer, QString path) :
     QDialog(parent),
     ui(new Ui::PrintDialog),
     m_path(path)
 {
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 1";
+
     //  set up the UI
     ui->setupUi(this);
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 2";
 
     //  set the window title
     setWindowTitle(tr("Print Document"));
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 3";
 
     //  insure we've got the arrow cursor.
     qApp->setOverrideCursor(Qt::ArrowCursor);
     qApp->processEvents();
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 4";
 
     //  start with all pages
     ui->allRadioButton->setChecked(true);
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 5";
 
     //  fill in printer names
     //  pre-select the current default printer
     QPrinterInfo defaultInfo = QPrinterInfo::defaultPrinter();
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 6";
     ui->printerCombo->blockSignals(true);
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 7";
     m_printerList=QPrinterInfo::availablePrinters();
     int ndx = 0;
     foreach (QPrinterInfo printerInfo, m_printerList)
     {
+        QTextStream(stdout) << "\n" << "PrintDialog ctor 8, ndx=" << ndx;
         ui->printerCombo->addItem(printerInfo.description());
         if (printerInfo.description() == defaultInfo.description())
             ui->printerCombo->setCurrentIndex(ndx);
+        QTextStream(stdout) << "\n" << "PrintDialog ctor 9";
         ndx++;
     }
     ui->printerCombo->blockSignals(false);
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 10";
 
     //  printer
     m_printer = printer;
     onNewPrinter();
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 11";
 
     //  MuPDF document (for rendering preview)
     m_document = new Document();
     m_document->Initialize();
     m_document->OpenFile(m_path.toStdString().c_str());
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 12";
 
     //  set up slider based on the number of pages
     m_currentPage = currentPage;
     m_maxPages = maxPages;
     setupSlider();
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 13";
 
     //  portait by default
     ui->portraitRadio->setChecked(true);
 
     on_paperSizeComboBox_currentIndexChanged(0);
+    QTextStream(stdout) << "\n" << "PrintDialog ctor 14";
 }
 
 int PrintDialog::countPrinters()
@@ -183,9 +199,12 @@ void PrintDialog::setSliderLabel(int val)
 
 void PrintDialog::setupSlider()
 {
+    QTextStream(stdout) << "\n" << "setupSlider 1";
     QString range = this->printRange();
     QList<int> pageList = Printer::listFromRange(range, m_maxPages);
     int np = pageList.size();
+
+    QTextStream(stdout) << "\n" << "setupSlider 2";
 
     if (np<=1)
     {
@@ -198,7 +217,11 @@ void PrintDialog::setupSlider()
         ui->pageSlider->setEnabled(true);
     }
 
+    QTextStream(stdout) << "\n" << "setupSlider 3";
+
     setSliderLabel(1);
+
+    QTextStream(stdout) << "\n" << "setupSlider 4";
 }
 
 void PrintDialog::updatePreview()
@@ -221,6 +244,8 @@ void PrintDialog::onPreviewTimer()
 
 void PrintDialog::renderPreview()
 {
+    QTextStream(stdout) << "\n" << "renderPreview 1";
+
     //  clear the preview
     ui->previewLabel->clear();
 
@@ -230,12 +255,14 @@ void PrintDialog::renderPreview()
     if (pageList.isEmpty())
         return;  //  no pages
     int pageNumber = pageList.at(ui->pageSlider->value()-1)-1;
+    QTextStream(stdout) << "\n" << "renderPreview 2";
 
     //  get the document page dimensions (inches) @ 72DPI
     point_t pageSize;
     m_document->GetPageSize(pageNumber, 1.0, &pageSize);
     double pageWidth = pageSize.X/72.0;
     double pageHeight = pageSize.Y/72.0;
+    QTextStream(stdout) << "\n" << "renderPreview 3";
 
     //  get current paper size (inches)
     double paperWidth = m_paperWidth;
@@ -264,10 +291,12 @@ void PrintDialog::renderPreview()
     py +=15;
     ph -=15;
 
+    QTextStream(stdout) << "\n" << "renderPreview 4";
     ui->previewLabel->setGeometry(px,py,pw,ph);
 
     //  fill widget with white
     ui->previewLabel->setStyleSheet("QLabel { background-color : white; color : white; }");
+    QTextStream(stdout) << "\n" << "renderPreview 5";
 
     //  autofit
     ui->previewLabel->setAlignment(Qt::AlignTop|Qt::AlignLeft);
@@ -308,6 +337,8 @@ void PrintDialog::renderPreview()
     }
     scale = scale * m_autoFitScale;
 
+    QTextStream(stdout) << "\n" << "renderPreview 6";
+
     //  render the page to an image
     int w = scale*pageSize.X;
     int h = scale*pageSize.Y;
@@ -319,6 +350,8 @@ void PrintDialog::renderPreview()
     m_document->RenderPage (pageNumber, scale, m_bitmap, w, h, false);
     m_image = new QImage(m_bitmap, w, h, QImage::Format_ARGB32);
 
+    QTextStream(stdout) << "\n" << "renderPreview 7";
+
     //  rotate
     if (m_bAutoFitRotate)
     {
@@ -327,10 +360,14 @@ void PrintDialog::renderPreview()
         *m_image = m_image->transformed(tf);
     }
 
+    QTextStream(stdout) << "\n" << "renderPreview 8";
+
     //  set it in the widget
     m_pixmap = QPixmap::fromImage(*m_image);
     ui->previewLabel->setPixmap(m_pixmap);
     ui->previewLabel->update();
+
+    QTextStream(stdout) << "\n" << "renderPreview 9";
 
     //  set the geomatry for the size labels.
     //  these get drawn in updateSizeDisplay()
@@ -339,6 +376,9 @@ void PrintDialog::renderPreview()
 
     //  update size display
     updateSizeDisplay();
+
+    QTextStream(stdout) << "\n" << "renderPreview 10";
+
 }
 
 void PrintDialog::onClose()
@@ -365,8 +405,11 @@ void PrintDialog::onClose()
 
 void PrintDialog::onNewPrinter()
 {
+    QTextStream(stdout) << "\n" << "onNewPrinter 1";
+
     //  info for the currently selected printer.
     QPrinterInfo printerInfo = m_printerList.at(ui->printerCombo->currentIndex());
+    QTextStream(stdout) << "\n" << "onNewPrinter 2";
 
     //  remember the current page size name
     //  this is a bit of a hack since we really should get the current default
@@ -382,12 +425,14 @@ void PrintDialog::onNewPrinter()
 #endif
     else
         currentSizeName = "A4";
+    QTextStream(stdout) << "\n" << "onNewPrinter 3";
 
     if (m_paperSizes.size()>0 && ui->paperSizeComboBox->currentIndex()<m_paperSizes.size())
     {
         QPair<QString,QSizeF> paperSize = m_paperSizes.at(ui->paperSizeComboBox->currentIndex());
         currentSizeName = paperSize.first;
     }
+    QTextStream(stdout) << "\n" << "onNewPrinter 4";
 
     //  get the new paper sizes
     ui->paperSizeComboBox->blockSignals(true);
@@ -396,6 +441,8 @@ void PrintDialog::onNewPrinter()
     int matching = -1;
     for (int i=0; i<m_paperSizes.size() ;i++)
     {
+        QTextStream(stdout) << "\n" << "onNewPrinter 5, i = " <<  i;
+
         QPair<QString,QSizeF> paperSize = m_paperSizes.at(i);
         ui->paperSizeComboBox->addItem(paperSize.first);
 
@@ -407,12 +454,15 @@ void PrintDialog::onNewPrinter()
     if (matching!=-1)
         ui->paperSizeComboBox->setCurrentIndex(matching);
 
+    QTextStream(stdout) << "\n" << "onNewPrinter 6";
+
     on_paperSizeComboBox_currentIndexChanged(ui->paperSizeComboBox->currentIndex());
 
     ui->paperSizeComboBox->blockSignals(false);
 
     //  set the printer name
     m_printer->setPrinterName(printerInfo.printerName());
+    QTextStream(stdout) << "\n" << "onNewPrinter 7";
 }
 
 QString formatDimension(double val)
@@ -447,6 +497,8 @@ void drawText(QPainter & painter, qreal x, qreal y, int flags,
 
 void drawSizeLabel(QLabel *label, double value, bool rotate)
 {
+    QTextStream(stdout) << "\n" << "drawSizeLabel 1";
+
     label->clear();
 
     int w = label->width();
@@ -480,10 +532,14 @@ void drawSizeLabel(QLabel *label, double value, bool rotate)
     }
 
     label->setPixmap( pixmap );
+
+    QTextStream(stdout) << "\n" << "drawSizeLabel 2";
 }
 
 void PrintDialog::updateSizeDisplay()
 {
+    QTextStream(stdout) << "\n" << "updateSizeDisplay 1";
+
     //  get current paper size (inches)
     double paperWidth = m_paperWidth;
     double paperHeight = m_paperHeight;
@@ -503,6 +559,9 @@ void PrintDialog::updateSizeDisplay()
 
     drawSizeLabel(ui->widthLabel,  paperWidth, false);
     drawSizeLabel(ui->heightLabel, paperHeight, true);
+
+    QTextStream(stdout) << "\n" << "updateSizeDisplay 2";
+
 }
 
 void PrintDialog::on_pageSlider_valueChanged(int value)
