89f8aedb3b3383b94c1061b56de72b915ee5fb30
Fred Ross-Perry <fredrossperry@gmail.com>
2015-06-11 17:22:15 -0700

    add USerGuide.pdf to the Linux 64 build

--- linux-deploy/build-installer.sh
+++ linux-deploy/build-installer.sh
@@ -7,8 +7,10 @@ cd "$this_dir"
 # get latest built apps
 rm -f ./packages/com.artifex.gsview/data/gsview.exe
 rm -rf ./packages/com.artifex.gsview/data/apps
+rm -rf ./packages/com.artifex.gsview/data/UserGuide.pdf
 cp ../build/release/gsview ./packages/com.artifex.gsview/data/gsview.exe
 cp -r ../build/release/apps ./packages/com.artifex.gsview/data/apps
+cp ../build/release/UserGuide.pdf ./packages/com.artifex.gsview/data/UserGuide.pdf
 
 #  patch the exe and set permissions
 patchelf --set-interpreter ./libs/ld-linux-x86-64.so.2 ./packages/com.artifex.gsview/data/gsview.exe 
